34aaa6df6607ff5e4b97f7d69c9286512cc38f84
illumination <illuminationth@gmail.com>
2018-12-23 13:08:49 +0900

    砂場を入れた

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
@@ -7,17 +8,80 @@ public class MapGenerator : MonoBehaviour {
 
     [SerializeField] private TextAsset[] m_mapData;
 
-    [SerializeField] private GameObject m_space;
-    [SerializeField] private GameObject m_boss;
+    [SerializeField] private GameObject m_dragonWorld;
+    [SerializeField] private GameObject[] m_enemiesWorld;
+    [SerializeField] private GameObject[] m_bossesWorld;
+    //ｱｲﾃﾑ
 
+    //ドラゴンの初期位置はここで調整してください
+    //後でCSVファイルの中に入れるかも
+    [SerializeField] private Vector2[] m_dragonInitPos;
 
+    [NonSerialized]public DragonWorld Dragon = null;
+    [NonSerialized] public List<EnemyWorld> EnemyList = new List<EnemyWorld>();
+    [NonSerialized] public Vector3 MapSize = Vector3.zero;
+
+    //ちょっとおいとく（使っていない)
     public void MakeMap(int mapID)
     {
         StringReader stringReader = new StringReader(m_mapData[mapID].text);
 
+        //最初は敵の生成（ボス含む)
         
+        int x = 0, z = 0,zMax = 0;
+        //ExcelのCSVファイルを1行ずつ切り取る
+        while (stringReader.Peek() > -1)
+        {
+            string col = stringReader.ReadLine();
+            //","で区切る。
+            string[] data = col.Split(',');
+            
+            foreach(string datum in data)
+            {
+                GameObject gridObj = GetObjectFromData(datum);
+                if (gridObj != null)
+                {
+                    EnemyWorld e = gridObj.GetComponent<EnemyWorld>();
+                    e.GridPosition = new Vector3(x, 0, z);
+                    e.SetPosition();
+                    EnemyList.Add(e);
+                }
+                z++;
+            }
+            x++;
+            zMax = z;
+            z = 0;
 
+        }
+        MapSize = new Vector3(x, 0, zMax);
 
+        //プレイヤーの生成
+        Dragon = Instantiate(m_dragonWorld).GetComponent<DragonWorld>();
+        m_dragonWorld.GetComponent<DragonWorld>().GridPosition = m_dragonInitPos[mapID];
 
     }
+    
+    //エクセルに入れた文字によって生成するオブジェクト（敵の種類、ボスの種類)を返します
+    private GameObject GetObjectFromData(string datum)
+    {
+        GameObject obj = null;
+        switch (datum)
+        {
+            case "0":break;
+            case "e0":obj = m_enemiesWorld[0];break;
+            case "b0":obj = m_bossesWorld[0];break;
+            default:
+                Debug.LogError("Excelファイルに変な値が書き込まれています");
+                break;
+        }
+        if (obj != null)
+        {
+            return Instantiate(obj);
+        }
+        else
+        {
+            return null;
+        }
+    }
+
 }
--- Assets/Scripts/WorldManager.cs
+++ Assets/Scripts/WorldManager.cs
@@ -2,16 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WorldManager : MonoBehaviour {
+public partial class WorldManager : MonoBehaviour
+{
+    [SerializeField] private MapGenerator m_mapGenerator;
 
     [SerializeField] private string m_BattleSceneName;
-
+    [SerializeField] private string m_TitleSceneName;
     private SceneSequencer sceneSequencer;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
 
     // Use this for initialization
     void Start()
     {
         sceneSequencer = GameObject.Find("SceneSequencer").GetComponent<SceneSequencer>();
+
+        m_mapGenerator.MakeMap(0);
+        Initialize(m_mapGenerator);
     }
 
     // Update is called once per frame
@@ -21,5 +31,49 @@ public class WorldManager : MonoBehaviour {
         {
             sceneSequencer.ChangeScene(m_BattleSceneName);
         }
+
+        Execute();
+
+        //ドラゴンと敵が同じグリッドに来たらバトルへGO
+        if (DragonMeetsEnemy())
+        {
+            sceneSequencer.ChangeScene(m_BattleSceneName);
+        }
     }
+
+
+    //ここでドラゴン、敵、ボスの座標管理（→シーン遷移）
+    private List<EnemyWorld> m_enemies = new List<EnemyWorld>();
+    private DragonWorld m_dragon = null;
+    private Vector3 m_mapSize;
+    public void Initialize(MapGenerator generator)
+    {
+        m_dragon = generator.Dragon;
+        m_enemies = generator.EnemyList;
+        m_mapSize = generator.MapSize;
+    }
+
+    private void Execute()
+    {
+        m_dragon.Execute(m_mapSize);
+        foreach (EnemyWorld enemy in m_enemies)
+        {
+            enemy.Execute(m_mapSize);
+        }
+    }
+
+    private bool DragonMeetsEnemy()
+    {
+        Vector3 dragonPos = m_dragon.GridPosition;
+        foreach(EnemyWorld enemy in m_enemies)
+        {
+            if (enemy.GridPosition == dragonPos)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
